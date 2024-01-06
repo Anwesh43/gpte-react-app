715213833426d0c46cfa06e25e44dc09d1ab198c
Anwesh43 <anweshthecool0@gmail.com>
2024-01-06 13:57:40 +0530

    using card container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo, useEffect } from 'react';
+import CardContainer, { CardData } from './components/CardContainer';
+import storeApi, { Product } from './services/storeApi';
 
 function App() {
+  const [toggle, setToggle] = useState(false)
+  const [cards, setCards] = useState<CardData[]>([])
+  const storeApiService = useMemo(() => storeApi(), [])
+  useEffect(() => {
+    
+    const getCards = async () => {
+      const products = await storeApiService.getProductCards()
+      console.log("Products", products)  
+      setCards(products.map((p : Product) => ({
+        title: p.title, 
+        imageUrl: p.image, 
+        subtitle: p.description
+      })))
+    }
+    getCards()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full flex items-center flex-col justify-start'>
+      <div onClick = {() => {
+        setToggle(!toggle)
+      }} className={`transition-colors rounded-sm bg-green-500 hover:bg-orange-500 w-full h-[30px] flex justify-center items-center ${toggle ? 'animate-spin' : ''}`}>
+        Products
+      </div>
+      <CardContainer cards={cards}/>
     </div>
   );
 }
